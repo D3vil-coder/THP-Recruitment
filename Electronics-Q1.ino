13655b92f3d57c4204618b20dec78c22cc89a3a5
D3vil-coder <whitedevilxtra@gmail.com>
2024-10-12 00:50:43 +0530

    Create Electronics-Q1.ino
    This is the Tinker CAD code for the First Question of Electronics Sub-Division Assignment [Understanding Embedded Systems with Arduino].

---
+++ Electronics-Q1.ino
@@ -0,0 +1,81 @@
+#include <LiquidCrystal.h>
+
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+void setup()
+{
+  lcd.begin(16, 2);
+  lcd.setCursor(0, 0);
+  // Print a message to the LCD.
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop()
+{
+  lcd.setCursor(0, 0);
+  lcd.print("Input: ");
+  checkinputt();
+  convtobin();
+  delay(500);
+  lcd.clear();
+}
+
+
+void checkinputt()
+{
+    if(digitalRead(10)==1)
+  {
+    lcd.print("1");
+  }
+  else if(digitalRead(10)==0)
+  {
+	lcd.print("0");
+  }
+  if(digitalRead(9)==1)
+  {
+    lcd.print("1");
+  }
+  else if(digitalRead(9)==0){
+	lcd.print("0");
+  }
+  if(digitalRead(8)==1)
+  {
+    lcd.print("1");
+  }
+  else if(digitalRead(8)==0){
+	lcd.print("0");
+  }
+  if(digitalRead(7)==1)
+  {
+    lcd.print("1");
+  }
+  else if(digitalRead(7)==0){
+	lcd.print("0");
+  }
+
+}
+
+void convtobin()
+{
+ int num = 0;
+  	if(digitalRead(10)==1)
+    {
+     num+=8; 
+    }
+  if(digitalRead(9)==1)
+    {
+     num+=4; 
+    }
+  if(digitalRead(8)==1)
+    {
+     num+=2; 
+    }
+  if(digitalRead(7)==1)
+    {
+     num+=1; 
+    }
+  	lcd.setCursor(0,1);
+    lcd.print("Output: ");
+	lcd.print(num);
+}
